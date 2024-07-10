3641b42fd971f76d95fdef5bdca21ddc9b27c049
KeeanBall <balsigke@oregonstate.edu>
2024-07-10 14:29:54 -0700

    project 3c

---
+++ num_guess.py
@@ -0,0 +1,17 @@
+print("Enter the integer for the player to guess.")
+num = int(input())
+guessed_right = False
+print("Enter your guess.")
+guess = int(input())
+num_guesses = 1
+while not guessed_right:
+    if guess > num:
+        print("Too high - try again:")
+        guess = int(input())
+    elif guess < num:
+        print("Too low - try again:")
+        guess = int(input())
+    else:
+        guessed_right = True
+    num_guesses += 1
+print(f"You guessed it in {num_guesses} tries.")
